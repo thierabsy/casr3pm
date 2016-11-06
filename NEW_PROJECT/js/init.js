1339638905dc75a978ce31143df41472c2074d97
thierabsy <thierabsy@yahoo.fr>
2016-11-06 21:58:13 +0000

    PwgSlider added to our starter template

--- NEW_PROJECT/js/init.js
+++ NEW_PROJECT/js/init.js
@@ -3,5 +3,15 @@
 
     $('.button-collapse').sideNav();
 
+    $('.carousel.carousel-slider').carousel({full_width: true});
+
+     $('.slider').slider({full_width: true});
+
+     $('.pgwSlider').pgwSlider({
+    maxHeight : 400,
+    intervalDuration : 4000
+}
+	);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
