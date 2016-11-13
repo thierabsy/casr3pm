afd901dce98da642b42d85cf5995adf0cb9c72f9
thierabsy <thierabsy@yahoo.fr>
2016-11-13 02:28:39 +0000

    Beta

--- js/init.js
+++ js/init.js
@@ -17,5 +17,16 @@
 
     $('.tooltipped').tooltip({delay: 50});
 
+    // OWL
+   $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 6,
+      // itemsDesktop : [1199,3],
+      // itemsDesktopSmall : [979,3]
+ 
+  });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
